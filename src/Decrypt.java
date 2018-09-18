2d1a9bb044584af5cb3a3ed1ad7805e0774ff8b5
chong050 <chong050@morris.umn.edu>
2018-09-17 19:39:11 -0500

    Created frequencyInitialize to keep track of letter frequency of English to use later in comparing in order to find key length.

--- src/Decrypt.java
+++ src/Decrypt.java
@@ -1,14 +1,60 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 
 public class Decrypt {
+
+    public HashMap<Character, Double> engHash = new HashMap<>();
+    public LetterFrequency englishFrequencies;
+
+    public void frequencyIntialize(){
+        engHash.put('a', 8.167);
+        engHash.put('b', 1.492);
+        engHash.put('c', 2.782);
+        engHash.put('d', 4.253);
+        engHash.put('e', 12.702);
+        engHash.put('f', 2.228);
+        engHash.put('g', 2.015);
+        engHash.put('h', 6.094);
+        engHash.put('i', 6.966);
+        engHash.put('j', 0.153);
+        engHash.put('k', 0.772);
+        engHash.put('l', 4.025);
+        engHash.put('m', 2.406);
+        engHash.put('n', 6.749);
+        engHash.put('o', 7.507);
+        engHash.put('p', 1.929);
+        engHash.put('q', 0.095);
+        engHash.put('r', 5.987);
+        engHash.put('s', 6.327);
+        engHash.put('t', 9.056);
+        engHash.put('u', 2.758);
+        engHash.put('v', 0.978);
+        engHash.put('w', 2.360);
+        engHash.put('x', 0.150);
+        engHash.put('y', 1.974);
+        engHash.put('z', 0.074);
+        englishFrequencies = new LetterFrequency(engHash);
+    }
+
+
+
+    public void decrypter(String encryptedText){
+        frequencyIntialize();
+        LetterFrequency[] freqArray = (LetterFrequency[]) new Object[8];
+        for(int i = 0; i < 8; i++){
+                freqArray[i] = new LetterFrequency(encryptedText, i + 1);
+        }
+        Arrays.sort(freqArray, new FrequencyComparator());
+
+    }
 
     private HashMap<Character, Double> getFrequencies(String s, int n){
 
         char[] charArray = s.toCharArray();
         int stringLength = s.length();
         double stringFraction = 1/stringLength;
-        Tuple<Character, Double>[] freqArray = new Tuple<Character, Double>[];
+        Tuple<Character, Double>[] freqArray = (Tuple[])new Object[]{null,null};
         for(int i = 0; i < 26; i++){
             freqArray[i] = new Tuple<>((char)('a' + i),0.0);
         }
@@ -43,14 +89,14 @@ public class Decrypt {
         }
     }
 
-    private class letterFrequency {
+    private class LetterFrequency {
         public HashMap<Character, Double> frequencies;
 
-        public letterFrequency(String s, int n){
+        public LetterFrequency(String s, int n){
             frequencies = getFrequencies(s, n);
         }
 
-        public letterFrequency(HashMap<Character, Double> h){
+        public LetterFrequency(HashMap<Character, Double> h){
             frequencies = h;
         }
 
@@ -63,23 +109,31 @@ public class Decrypt {
         //If the two letterFrequencies have different numbers of characters in them,
         //the function will simply return no correlation.
 
-        public double frequencyNearness(letterFrequency first, letterFrequency second){
-            if(first.size() != second.size()){
+        public double frequencyNearness(LetterFrequency second){
+            if(this.size() != second.size()){
                 return 101; //TODO approximate missing characters as 0 to allow for comparison
             }
             double totalDifference = 0;
-            Integer[] firstValues = (Integer[])first.frequencies.values().toArray();
+            Integer[] firstValues = (Integer[])this.frequencies.values().toArray();
             Integer[] secondValues = (Integer[])second.frequencies.values().toArray();
             Arrays.sort(firstValues);
             Arrays.sort(secondValues);
             for(int i = 0; i < firstValues.length; i++){
                 double largest = Math.max(firstValues[i], secondValues[i]);
                 double difference = Math.abs(firstValues[i] - secondValues[i]);
-                double percentDifference = ((largest / difference) * 100)/first.size();
+                double percentDifference = ((largest / difference) * 100)/this.size();
                 totalDifference += percentDifference;
             }
             return totalDifference;
         }
     }
+
+    private class FrequencyComparator implements Comparator<LetterFrequency>{
+        @Override
+        public int compare(LetterFrequency o1, LetterFrequency o2) {
+            return o1.frequencyNearness(englishFrequencies) - o2.frequencyNearness(englishFrequencies);
+        }
+    }
+
 
 }
